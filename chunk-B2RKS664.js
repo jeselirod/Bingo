286e14f48d45710bb5205dfb206cdda606d83622
jeselirod <39128879+jeselirod@users.noreply.github.com>
2025-06-07 17:23:30 +0000

    Deploying to gh-pages from @ jeselirod/Bingo@7ef4821425e8a78948a17bdc504c8e9b2f6c4891 🚀

--- chunk-B2RKS664.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-2NFLSA4Y.js";var t=[{path:"",loadComponent:()=>import("./chunk-CIXF62WW.js").then(o=>o.AdminBingo)}];export{t as routesBingo};
---
+++ chunk-YRPXQ3TX.js
@@ -0,0 +1,1 @@
+import"./chunk-2NFLSA4Y.js";var t=[{path:"",loadComponent:()=>import("./chunk-FKM4TL5X.js").then(o=>o.AdminBingo)}];export{t as routesBingo};
